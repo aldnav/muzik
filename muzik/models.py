0cb84a7adcbb217227c54c10224970c07bdf8bb7
Aldrin Navarro <aldrinnavarro16@gmail.com>
2017-09-29 09:44:35 +0800

    Dj app setup

---
+++ muzik/models.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=254)
+    gender = models.CharField(max_length=7, blank=True)
+    disambiguation = models.CharField(max_length=64, blank=True)
+    bio = models.TextField(blank=True)
+    area = models.CharField(max_length=64, blank=True)
+    play_count = models.IntegerField(null=True, blank=True)
+    listener_count = models.IntegerField(null=True, blank=True)
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=254)
+    release_date = models.DateField(null=True, blank=True)
+    play_count = models.IntegerField(null=True, blank=True)
+    listener_count = models.IntegerField(null=True, blank=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=128)
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=254)
+    artist = models.ForeignKey(
+        Artist, related_name='songs', null=True, blank=True)
+    album = models.ForeignKey(
+        Album, related_name='songs', null=True, blank=True)
+    duration = models.IntegerField(null=True, blank=True)
+    stream_count = models.IntegerField(null=True, blank=True)
+    listener_count = models.IntegerField(null=True, blank=True)
+    play_count = models.IntegerField(null=True, blank=True)
+    tags = models.ManyToManyField(Tag)
---
+++ muzik/utils.py
@@ -0,0 +1,109 @@
+import csv
+import musicbrainzngs as mb
+import requests
+import pylast
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+from django.conf import settings
+
+from .models import Album, Artist, Song, Tag
+
+spotifycharts = "https://spotifycharts.com/regional/global/daily/latest/download"
+STREAMS_INPUT = settings.APP_STREAMS_CSV
+mb.set_useragent('songs', '1.0', 'http://example.com')
+network = pylast.LastFMNetwork(
+    api_key=settings.LF_API_KEY, api_secret=settings.LF_API_SECRET,
+    username=settings.LF_API_USERNAME,
+    password_hash=pylast.md5(settings.LF_API_PASSWORD))
+
+
+def request_stream_list():
+    # save csv file
+    r = requests.get(spotifycharts)
+    if r.status_code == 200:
+        with open(STREAMS_INPUT, 'w') as f:
+            f.write(r.text)
+
+
+def insert_from_csv():
+    with open(STREAMS_INPUT, 'rb') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            artist, _ = Artist.objects.get_or_create(
+                name=row['Artist']
+            )
+            Song.objects.get_or_create(
+                title=row['Track Name'],
+                stream_count=row['Streams'],
+                artist=artist
+            )
+
+def get_info_from_musicbrainz():
+    for artist in Artist.objects.all():
+        results = {}
+        try:
+            results = mb.search_artists(artist.name)
+        except Exception as e:
+            print e
+        else:
+            if results['artist-count'] > 0:
+                # get the result with the highest lucene score
+                result = max(
+                        results['artist-list'],
+                        key=lambda x: int(x['ext:score']))
+                artist.gender = result.get('gender', '')
+                artist.disambiguation = result.get('disambiguation', '')
+                area = result.get('area', '')
+                if area and 'name' in area:
+                    artist.area = area['name']
+                # tags = result.get('tag-list', [])
+                # @NOTE what to do with tags
+                artist.save()
+
+
+def get_info_from_last_fm():
+    for song in Song.objects.all():
+        track = network.get_track(song.artist.name, song.title)
+        if track:
+            l_duration = track.get_duration()
+            l_listener_count = track.get_listener_count()
+            l_play_count = track.get_playcount()
+            song.duration = l_duration
+            song.listener_count = l_listener_count
+            song.play_count = l_play_count
+
+            try:
+                l_album = track.get_album()
+                album, _ = Album.objects.get_or_create(
+                    title=l_album.title)
+                if album:
+                    album.release_date = l_album.get_release_date()
+                    album.listener_count = l_album.get_listener_count()
+                    album.play_count = l_album.get_playcount()
+            except Exception as e:
+                print e
+            else:
+                album.save()
+                song.album = album
+            song.save()
+
+        l_artist = network.get_artist(song.artist.name)
+        if l_artist:
+            song.artist.bio = str(l_artist.get_bio_summary())
+            song.artist.listener_count = l_artist.get_listener_count()
+            song.artist.play_count = l_artist.get_playcount()
+            song.artist.save()
+
+
+def main():
+    # request_stream_list()
+    insert_from_csv()
+    get_info_from_musicbrainz()
+    get_info_from_last_fm()
+
+
+if __name__ == '__main__':
+    main()
